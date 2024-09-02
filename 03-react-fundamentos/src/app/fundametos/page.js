d7a67e11740bb095b318e0cae4df815eb66a5db7
albuquerque-joao <albuquerquejoao.br@gmail.com>
2024-09-02 20:26:23 -0300

    adicionando o react no projeto

---
+++ 03-react-fundamentos/src/app/fundametos/page.js
@@ -0,0 +1,9 @@
+const numero = 2 + 2
+export default function Fundamentos() {
+    return(
+        <div>
+            <h1>{numero}</h1>
+            <p>Um  texto qualquer</p>
+        </div>
+    ) 
+}
