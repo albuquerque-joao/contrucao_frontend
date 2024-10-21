4f0e33c53ff8e92ecc4f435a34a90ac045218c71
albuquerque-joao <albuquerquejoao.br@gmail.com>
2024-10-21 13:03:25 -0300

    Criando a pasta componets e ajustando o cabeçalho e preparando para o codigo do formulario

---
+++ exercicios/atividade_08/cadastro_imoveis/src/app/page.js
@@ -0,0 +1,10 @@
+import Pagina from "@/components/page";
+
+export default function HomePage() {
+  return (
+    <Pagina titulo="Cadastro de Imoveis">
+      <p>Home Page</p>
+
+    </Pagina>
+  );
+}
